7ecf55fa3a8066f4c2c5bf59bb963079b9091947
Anup Adhikari <67635265+anup1009@users.noreply.github.com>
2023-05-20 13:23:36 +0545

    modeltest

--- app/app.py
+++ app/app.py
@@ -1,14 +1,35 @@
 from flask import Flask, jsonify, request
-from torch_utils import transform_image
 from flask_cors import CORS, cross_origin
-import io
+
 from PIL import Image
-from torch_utils import get_prediction
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
+
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+#with open('/home/anup/flaskapi/app/model.json', 'r') as json_file:
+    #global loaded_model_json 
+    #loaded_model_json = json_file.read()
+#model = model_from_json(loaded_model_json)
+#model.load_weights('/home/anup/flaskapi/app/weights.h5')
+#model.init()
 
+import torch_utils as load_model
+global model, graph
+model, graph = load_model.init()
+model=load_model("/home/anup/flaskapi/app/model.json")
+from skimage import io
+
+def generate_input_image(img):
+    
+    im_resized = img.resize((480, 480))
+    X = im_resized.reshape(1, 480, 480, 3)
+    print(X)
+    X = X/255.0
+    return X
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 def allowed_file(filename):
@@ -27,12 +48,16 @@ def predict_image():
             return jsonify({'error': 'format not supported'})
 
         try:
-            img_bytes = file.read()
-            img = Image.open(io.BytesIO(img_bytes))
-            prediction = get_prediction(img)
-            data =  {'prediction': prediction}
-            return jsonify(data)
-        except:
+            formFile = request.files['file']
+            img = io.imread(formFile)
+            X = generate_input_image(img)
+            with graph.as_default():
+                prediction = model.predict(X)
+                print(prediction)
+                pred_value = prediction.flatten().tolist()[0]
+                return jsonify({ "prediction": pred_value })
+        except e:
+            print(e)
             return jsonify({'error': 'error during prediction'})
         
     return jsonify({'result' : 1 })
--- app/torch_utils.py
+++ app/torch_utils.py
@@ -1,44 +1,20 @@
-import PIL
-import keras
-import numpy as np
+import os
+this_dir = os.path.dirname(__file__)
+
 from keras.models import model_from_json
-import math
+import tensorflow as tf
 
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-model.load_weights("weights.h5")
+def path(file):
+	return os.path.join(this_dir, file)
 
-def get_prediction(img):
-    height = 480 - img.shape[0]
-    width = 480 - img.shape[1]
+def init():
+  with open(path('model.json')) as data_file:
+    loaded_model = model_from_json(data_file.read())
+    loaded_model.load_weights(path('weights.h5'))
+    print('Loaded model!')
 
-    if(height % 2 == 1 & width % 2 == 1):
-        height1,height2 = math.floor(height/2), math.floor(height/2) + 1
-        width1,width2 = math.floor(width/2), math.floor(width/2) +1
-    elif(width % 2 == 1):
-        width1,width2 = math.floor(width/2), math.floor(height/2) + 1
-        height1,height2 = int(height/2), int(height/2)
-    elif(height % 2 == 1):
-        height1,height2 = math.floor(height/2), math.floor(height/2) + 1
-        width1,width2 = int(width/2), int(width/2) 
-    else:
-        height1,height2 = int(height/2), int(height/2)
-        width1,width2 = int(width/2), int(width/2)
+    loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    graph = tf.compat.v1.get_default_graph()
 
-    if(height == 0):
-        img = np.lib.pad(img, ((0,0),(width1, width2),(0,0)), 'edge')
-    elif (width == 0):
-        img = np.lib.pad(img, ((height1, height2),(0,0),(0,0)), 'edge')
-    else:
-        img = np.lib.pad(img, ((height1, height2),(width1, width2),(0,0)), 'edge')
-    
-    x = img
-    x = x.astype(np.uint8)
-    x = x/ 255.0
-    x.shape = (1, ) + x.shape
-    p = model.predict(x)[0,0].round(2)
-    return str(p)
 
-    
+    return loaded_model, graph
